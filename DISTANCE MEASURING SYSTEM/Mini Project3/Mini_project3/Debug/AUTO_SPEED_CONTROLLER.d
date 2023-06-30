7791bcfa04a42222c650c708be6bcaa85a962dd7
Hagar Ahmed <85418088+Hagar123-cell@users.noreply.github.com>
2023-07-01 01:08:18 +0300

    Add files via upload

---
+++ DISTANCE MEASURING SYSTEM/Mini Project3/Mini_project3/Debug/AUTO_SPEED_CONTROLLER.d
@@ -0,0 +1,13 @@
+AUTO_SPEED_CONTROLLER.o AUTO_SPEED_CONTROLLER.o:  \
+ ../AUTO_SPEED_CONTROLLER.c ../DC_MOTOR.h ../std_types.h ../LCD.h \
+  ../TEMP_SENSOR.h ../ADC.h
+
+../DC_MOTOR.h:
+
+../std_types.h:
+
+../LCD.h:
+
+../TEMP_SENSOR.h:
+
+../ADC.h:
